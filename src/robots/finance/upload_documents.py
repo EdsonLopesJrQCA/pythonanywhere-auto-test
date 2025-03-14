64055705ce5556e4f5b062e6bf575942b21675df
Edson Lopes da Silva Júnior <edsonjunior@queirozcavalcanti.adv.br>
2025-03-14 09:49:01 -0300

    feat: implemented logging upload documents

--- src/robots/finance/upload_documents.py
+++ src/robots/finance/upload_documents.py
@@ -1,4 +1,15 @@
+import logging
 
 class UploadDocuments:
-    def execute():
-        print("RPA105 executado!")
+    def execute(self):
+        logging.info("Iniciando RPA105 - Upload de Documentos")
+        try:
+            # Simulação do processo do robô
+            logging.info("Executando processo de upload de documentos...")
+            
+            # Se ocorrer um erro, ele será capturado corretamente
+            # raise Exception("Falha no upload!")  # Simulação de erro
+
+            logging.info("RPA105 executado com sucesso!")
+        except Exception as e:
+            logging.error(f"Erro ao executar RPA105: {e}", exc_info=True)
